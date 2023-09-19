a8b85bbb2c95a2b41aed93f25b2c5154afe7913f
NaveenkumarMIT <naveen.gcebargur@gmail.com>
2023-09-19 18:39:06 +0530

    done

---
+++ B5.java
@@ -0,0 +1,12 @@
+class B5
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(3445123 / 10);
+		System.out.println(3445123 / 100);
+		System.out.println(3445123 / 1000);
+		System.out.println(3445123 / 10000);
+		System.out.println(3445123 / 100000);
+		System.out.println(3445123 / 1000000);
+	}
+}
